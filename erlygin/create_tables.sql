6e1c775ba4aca3efc677195208567ca7ccbb48ea
you@example.com <you@example.com>
2022-05-19 09:30:28 +0000

    tabels

--- erlygin/create_tables.sql
+++ erlygin/create_tables.sql
@@ -1,11 +1,75 @@
-CREATE TABLE params_fighter (
-    id SERIAL PRIMARY KEY,
-    fighter_id SERIAL REFERENCES fighter(id),
-    rel_coordinates FLOAT[3],
-    velocity FLOAT[3],
-    fuel_rate FLOAT,
-    weapon_rate FLOAT,
-    ts TIMESTAMP
+CREATE TABLE engineer (
+    engineer_id INT,
+    fio VARCHAR(255),
+    specialization VARCHAR(255),
+    PRIMARY KEY(engineer_id)
+);
+
+CREATE TABLE engineer_work_schedule (
+    engineer_id INT,
+    day VARCHAR(32),
+    PRIMARY KEY(engineer_id, day)
+    FOREIGN KEY(engineer_id) REFERENCES engineer(engineer_id) ON DELETE CASCADE
+);
+
+CREATE TABLE planes_specifications (
+    plane_model VARCHAR(32),
+    max_speed_km_per_hour INT,
+    max_lift_tonn INT,
+    max_practical_range INT,
+    num_inspections_per_year INT,
+    PRIMARY KEY(plane_model)
+);
+
+CREATE TABLE planes(
+    plane_number INT,
+    plane_model VARCHAR(32),
+    engineer_id INT REFERENCES engineer(engineer_id)
+    fuel_left_kg INT,
+    date_of_last_inspections DATE,
+    attached_fuel_tanks BOOLEAN,
+    ready_to_fight BOOLEAN,
+    PRIMARY KEY(plane_number),
+    FOREIGN KEY(plane_model) REFERENCES planes_specifications(plane_model) ON DELETE CASCADE
 );
 
 
+CREATE TABLE pilots(
+    pilot_id INT,
+    fio VARCHAR(255),
+    class VARCHAR(16),
+    call_sign VARCHAR(32),
+    on_duty BOOLEAN,
+    PRIMARY KEY(pilot_id)
+);
+
+CREATE TABLE pilot_info(
+    pilot_id INT,
+    last_medical_examination DATE,
+    experience_years INT,
+    vacation_data DATE,
+    PRIMARY KEY(pilot_id),
+    FOREIGN KEY(pilot_id) REFERENCES pilots(pilot_id) ON DELETE CASCADE
+);
+
+ALTER TABLE pilots
+ADD FOREIGN KEY(pilot_id)
+REFERENCES pilot_info(pilot_id)
+ON DELETE CASCADE
+
+CREATE TABLE weapon_on_planes(
+    plane_number INT,
+    weapon_name VARCHAR(16),
+    PRIMARY KEY(plane_number, weapon_name),
+    FOREIGN KEY(plane_number) REFERENCES planes(plane_number) ON DELETE CASCADE
+);
+
+CREATE plane_repair_history(
+    inspection_time DATETIME,
+    plane_number INT,
+    engineer_id INT,
+    report VARCHAR(32),
+    PRIMARY KEY(inspection_time),
+    FOREIGN KEY(plane_number) REFERENCES planes(plane_number) ON DELETE SET NULL
+    FOREIGN KEY(engineer_id) REFERENCES engineer(engineer_id) ON DELETE SET NULL
+);
--- init_bd.py
+++ init_bd.py
@@ -3,4 +3,5 @@ import subprocess
 
 if __name__ == '__main__':
     subprocess.run(['service', 'postgresql', 'start'])
+    subprocess.run(['dropdb', 'skp'])
     subprocess.run(['createdb', 'skp'])
